03686382e9d34821305be3da61ff1457725af936
wsrvn <vibex2new@gmail.com>
2023-08-14 06:48:43 +0300

    fix the syntax error in database creation query

--- plugins/autistic_plugins/Database.ts
+++ plugins/autistic_plugins/Database.ts
@@ -3,18 +3,18 @@ export { }
 const queries = [
   `CREATE TABLE IF NOT EXISTS trivia(
     player_id INT4 NOT NULL,
-    wins INT4 NOT NULL
+    wins INT4 NOT NULL,
     PRIMARY KEY(player_id)
     );`,
   `CREATE TABLE IF NOT EXISTS quotes(
     id INT4 GENERATED ALWAYS AS IDENTITY,
     quote VARCHAR(255) NOT NULL,
-    author VARCHAR(40) NOT NULL
+    author VARCHAR(40) NOT NULL,
     PRIMARY KEY(id)
     );`,
   `CREATE TABLE IF NOT EXISTS images(
     id INT4 GENERATED ALWAYS AS IDENTITY,
-    url VARCHAR(255) NOT NULL
+    url VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
     );`
 ]
